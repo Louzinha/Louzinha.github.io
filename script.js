3266b27e2fb14315df60b0c84f290ef781c3e297
Louzinha Carvalho <163380196+Louzinha@users.noreply.github.com>
2026-07-28 10:24:04 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("Welcome to Louzinha's PMO Portfolio");
+window.addEventListener("scroll", function () {
+
+const nav = document.querySelector("nav");
+
+if(window.scrollY > 50){
+
+nav.style.background="#16304d";
+
+}else{
+
+nav.style.background="#1E3A5F";
+
+}
+
+});
